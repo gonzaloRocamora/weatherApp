88e394d57c54873bbe7687f8168cfb4a35ab4e8b
gonzaloRocamora <gonza.rocamora@gmail.com>
2022-02-04 14:14:10 +0100

    sd

--- src/Forecast/Forecast.jsx
+++ src/Forecast/Forecast.jsx
@@ -6,5 +6,8 @@ export default function Forecast({dataForecast}) {
     console.log(data)
     return <div>
         <h1>{data.title}</h1>
+        <h4>{data.consolidated_weather[0].applicable_date}</h4>
+        <h3><span>Temperatura maxima:</span>{data.consolidated_weather[0].max_temp}</h3>
+        <h3><span>Temperatura minima:</span>{data.consolidated_weather[0].min_temp}</h3>
     </div>;
 }
